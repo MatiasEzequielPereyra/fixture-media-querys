d0641e0293de304f5fe754f02a01a337e92f480a
MatiasEzequielPereyra <108702482+MatiasEzequielPereyra@users.noreply.github.com>
2023-04-28 16:09:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,69 @@ iconList.addEventListener('click',function(){
     darkButton.classList.toggle('inactive')
 })
 });
+
+
+                                        
+                                                         //INTERACCION BOTONES DE LOS MESES 
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    var botonMayo = document.getElementById("botonMayo");
+    const mesMayo = document.getElementById("mayo");
+
+    botonMayo.addEventListener('click',function(){
+        mesMayo.classList.toggle('inactive')
+    })
+})
+
+   document.addEventListener("DOMContentLoaded",function(){
+    var   botonJunio = document.getElementById("botonJunio");
+    const mesJunio = document.getElementById("junio");
+
+    mesJunio.addEventListener('click',function(){
+        mesJunio.classList.toggle('inactive')
+    })
+   })
+
+   document.addEventListener("DOMContentLoaded",function(){
+    var myButton = document.getElementById("botonMayo");
+
+    myButton.addEventListener("click", function() {
+      myButton.classList.add("clicked");
+    });
+   })
+   document.addEventListener("DOMContentLoaded",function(){
+    var myButton = document.getElementById("botonJunio");
+
+    myButton.addEventListener("click", function() {
+      myButton.classList.toggle("clicked");
+    });
+   })
+   document.addEventListener("DOMContentLoaded",function(){
+    var myButton = document.getElementById("botonJulio");
+
+    myButton.addEventListener("click", function() {
+      myButton.classList.add("clicked");
+    });
+   })
+
+   document.addEventListener('DOMContentLoaded',function(){
+
+    function toggleClickedClass(button) {
+        var buttons = document.querySelectorAll(".clicked");
+      
+        buttons.forEach(function(otherButton) {
+          if (otherButton !== button && otherButton.classList.contains("clicked")) {
+            otherButton.classList.remove("clicked");
+          }
+        });
+      
+        button.classList.add("clicked");
+      }
+      
+        
+
+   })
+
+
+  //-----------------------------------------------------------------------------------------------------------------------------------------------
